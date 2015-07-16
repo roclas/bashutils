1d405a716b40ffc825e8be135ebfda21564fdcb5
carlos hernandez <carlos.hernandez@liferay.com>
2015-07-16 11:10:05 +0100

    Create notifyme-when-file-created.sh

---
+++ notifyme-when-file-created.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+inotifywait -m . -e create -e moved_to |
+    while read path action file; do
+        echo "The file '$file' appeared in directory '$path' via '$action'"
+        # do something with the file
+    done
