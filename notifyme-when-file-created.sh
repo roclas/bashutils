3882d563998f9ec756d252bc07d4f3ed51fdcefc
carlos hernandez <carlos.hernandez@liferay.com>
2016-04-11 10:31:43 +0100

    Update notifyme-when-file-created.sh
    -r for recursive searches in directories

--- notifyme-when-file-created.sh
+++ notifyme-when-file-created.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-inotifywait -m . -e create -e moved_to |
+inotifywait -r -m . -e create -e moved_to | #-e modify|
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
         # do something with the file
