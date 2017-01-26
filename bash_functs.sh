6028d9444f1e2201bff584ff45a61c851b7a9ad2
carlos <carlopolo@gmail.com>
2017-01-26 12:02:34 +0000

    bash functions improvement

--- bash_functs.sh
+++ bash_functs.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
 function grepInPDFs {
-	for i in $(find $2 -name "*pdf"|sort);do echo $i;pdftotext $i - | grep -i "$1"|sed "s/^/\t/";done
+	for i in $(find $2 -name "*pdf"|sort);do 
+		text=pdftotext $i - | grep -i "$1"|sed "s/^/\t/";
+		if [ ! -z "$text"  ];then
+			echo $i
+			pdftotext $i - | grep -i "$1"|sed "s/^/\t/";
+		fi
+	done
 }  
 
 function grepClassInJars {
-	for i in $(find $2 -name "*jar"|sort);do echo $i;jar -tf $i | grep -i "$1"|sed "s/^/\t/";done
+	for i in $(find $2 -name "*jar"|sort);do 
+		text=$(jar -tf $i | grep -i "$1"|sed "s/^/\t/")
+		if [ ! -z "$text"  ];then
+			echo $i
+			jar -tf $i | grep -i "$1"|sed "s/^/\t/"
+		fi
+	done
 }
+
+function searchword {
+        curl https://www.merriam-webster.com/dictionary/$1 | hxnormalize -x |hxselect '.card-primary-content'
+}
+
--- notifyme-when-file-created.sh
+++ notifyme-when-file-created.sh
@@ -3,5 +3,5 @@
 inotifywait -r -m . -e create -e moved_to | #-e modify|
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
-        # do something with the file
+	cp $path/$file ~/Desktop/
     done
